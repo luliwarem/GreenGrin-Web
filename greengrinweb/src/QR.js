a768afcd9fb725e807d6e02a7e8332aec5bbbf6c
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-06-12 12:07:54 -0300

    hay qr

--- greengrinweb/src/QR.js
+++ greengrinweb/src/QR.js
@@ -1,20 +1,24 @@
-import './QR.css';
-import {QRCode} from "react-native-custom-qr-codes"
+import "./QR.css";
+import QRCode from "react-qr-code";
 
 export default function QR() {
-
-    return(
-        <div className='Qr'>
-            <div className='Qr-header'>
-            <QRCode 
-            codeStyle='circle' 
-            innerEyeStyle='circle'
-            outerEyeStyle='circle'
-            linearGradient={['rgb(255,0,0)','rgb(0,255,255)']}
-            content="https://www.youtube.com/watch?v=UUeJjFB3fw8"
-            />
-            </div>
-        </div>
-
-    );
+  return (
+    <div className="Qr">
+      <div
+        style={{
+          height: "1000",
+          margin: "0 auto",
+          maxWidth: 64,
+          width: "100%",
+        }}
+      >
+        <QRCode
+          size={1000}
+          style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+          value="2, 3"
+          viewBox={`0 0 256 256`}
+        />
+      </div>
+    </div>
+  );
 }
