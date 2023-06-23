9c5328be2fa9592a4c23422b0a2df1fd2238a59d
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-06-23 12:11:10 -0300

    esta lindo

--- greengrinweb/src/QR.js
+++ greengrinweb/src/QR.js
@@ -4,11 +4,14 @@ import QRCode from "react-qr-code";
 export default function QR() {
   return (
     <div className="Qr">
+      <div>
+        <p>Escanee el código para acreditar sus puntos!</p>
+      </div>
       <div
         style={{
           height: "1000",
           margin: "0 auto",
-          maxWidth: 64,
+          maxWidth: 300,
           width: "100%",
         }}
       >
@@ -17,7 +20,16 @@ export default function QR() {
           style={{ height: "auto", maxWidth: "100%", width: "100%" }}
           value="2, 3"
           viewBox={`0 0 256 256`}
+          fgColor="#479A50"
         />
+      </div>
+
+      <div>
+        <p>Tiempo restante: 1:25 segundos </p>
+      </div>
+
+      <div className="buttonDiv" >
+        <button className="button">Finalizar</button>
       </div>
     </div>
   );
